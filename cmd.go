09f04a0855062f3f3fbd168d2713d343a748a59e
John Taylor <johntaylor@gmail.com>
2020-04-16 15:38:24 -0400

    initial commit

---
+++ cmd.go
@@ -0,0 +1,50 @@
+/*
+
+dls.go
+-John Taylor
+Apr-16-2020
+
+List files within a running docker container
+
+Adopted from:
+https://yourbasic.org/golang/list-files-in-directory/
+
+To statically compile the program on Linux:
+go build -tags netgo -ldflags '-extldflags "-static" -s -w'
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func main() {
+	table := tablewriter.NewWriter(os.Stdout)
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				modTime := fmt.Sprintf("%v", info.ModTime())
+				modTime = modTime[:19]
+				entry := []string{fmt.Sprintf("%9d", info.Size()), modTime, path}
+				table.Append(entry)
+			}
+
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+
+	table.SetHeader([]string{"Size", "Mod Time", "Name"})
+	table.Render()
+}
